c9a0fa7fb97066e7e8045cceeeff548bf2a9f258
PenguinHaHa <smilezjxu@gmail.com>
2014-08-11 21:23:10 +0800

    add hello module

---
+++ hello/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/penguin/WORKSPACE/linuxDriver/hello/hello.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-32-generic/scripts/module-common.lds --build-id  -o /home/penguin/WORKSPACE/linuxDriver/hello/hello.ko /home/penguin/WORKSPACE/linuxDriver/hello/hello.o /home/penguin/WORKSPACE/linuxDriver/hello/hello.mod.o
