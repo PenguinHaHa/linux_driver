c81eb397c5163c5adb8c3f8f9a2c1296dc873174
PenguinHaHa <smilezjxu@gmail.com>
2014-08-11 21:23:54 +0800

    add keyboard interrupt hook test

---
+++ kbdIntr/kbdintr.c
@@ -0,0 +1,34 @@
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+
+#define DEVICE_NAME "kbd_intr_hook"
+
+irqreturn_t hook_handler(int irq, void *dev_id)
+{
+  printk("kbd_hook: intr!\n");
+  return IRQ_HANDLED;
+}
+
+static int hook_init(void)
+{
+  int retval;
+  retval = request_irq(1,
+                       hook_handler,
+                       IRQF_SHARED | IRQF_DISABLED,
+                       "kbd_hook",
+                       (int*)0x55aa00ff);
+  printk("kbd_hook: retval=%d\n", retval);
+  return retval;
+}
+
+static void hook_exit(void)
+{
+  free_irq(1, (int*)0x55aa00ff);
+  printk("kbd_hook: exit\n");
+}
+
+module_init(hook_init);
+module_exit(hook_exit);
